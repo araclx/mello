5d23b7927e3331fe276e85b345598cbd70a7a72a
keinsell <keinsell@users.noreply.github.com>
2021-06-15 14:37:05 +0000

    style: auto-fix format issues

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,17 +1,15 @@
-import {Flex, Text, Box, Link} from 'rebass'
+import { Flex, Text, Box, Link } from 'rebass'
 
 function App() {
 	return (
-		<Flex
-		  px={2}
-		  color='white'
-		  bg='black'
-		  alignItems='center'>
-		  <Text p={2} fontWeight='bold'>Mello Public NotEvenAlpha 0.0.1-dev</Text>
-		  <Box mx='auto' />
-		  <Link variant='nav' color="white" href='#!'>
-		    Login
-		  </Link>
+		<Flex px={2} color='white' bg='black' alignItems='center'>
+			<Text p={2} fontWeight='bold'>
+				Mello Public NotEvenAlpha 0.0.1-dev
+			</Text>
+			<Box mx='auto' />
+			<Link variant='nav' color='white' href='#!'>
+				Login
+			</Link>
 		</Flex>
 	)
 }
--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {ThemeProvider} from '@emotion/react'
+import { ThemeProvider } from '@emotion/react'
 import theme from '@rebass/preset'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-
 ReactDOM.render(
 	<React.StrictMode>
-	<ThemeProvider theme={theme}>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
 		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
