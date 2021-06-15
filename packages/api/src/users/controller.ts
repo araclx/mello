72182c3884580d0c9d97e4cbe6ea17a1dfa57511
Jakub Olan <jakub.jay.olan@gmail.com>
2021-06-15 22:36:39 +0200

    chore: update logging and security

--- packages/api/src/users/controller.ts
+++ packages/api/src/users/controller.ts
@@ -4,7 +4,9 @@ import { ProfileAction } from './actions'
 export class ProfileController {
 	public async getAll(request: Request, response: Response) {
 		const action = new ProfileAction()
-		const data = await action.findProfiles()
+		const data = await action.returnAllProfilesFromDatabase()
 		response.json(data)
 	}
+
+	public async createProfile(request: Request, response: Response) {}
 }
---
+++ packages/api/src/utils/env.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import { isDevelopmentCheck } from './helpers'
+
+dotenv.config()
+
+// Core Configuration
+export const HOST = process.env.HOST! || 'localhost'
+export const PORT = Number.parseInt(process.env.MELLO_PORT!) || 1337
+
+// Auth0 Configuration
+export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN!
+export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE!
+
+export const NODE_ENV = process.env.NODE_ENV! || 'development'
+
+// ENV-based Lifecycle Variables
+export const isDevelopment = isDevelopmentCheck()
