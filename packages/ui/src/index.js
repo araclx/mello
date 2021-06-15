920df034008f1a2efd8033c7a9dfa5324c1424db
Jakub Olan <jakub.jay.olan@gmail.com>
2021-06-15 17:12:54 +0200

    refactor: add atomic design to react package (MEL-11)

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {ThemeProvider} from '@emotion/react'
+import ReactDOM, { render } from 'react-dom'
+import { ThemeProvider } from '@emotion/react'
 import theme from '@rebass/preset'
-import './index.css'
-import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from './organisms/app'
 import reportWebVitals from './reportWebVitals'
+import { AUTH0_DOMAIN, AUTH0_CLIENTID } from './utils/env'
 
+class Index extends React.Component {
+	render() {
+		return (
+			<React.StrictMode>
+				<Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENTID} redirectUri={window.location.origin}>
+					<ThemeProvider theme={theme}>
+						<Router>
+							<Route exact path='/' component={App} />
+						</Router>
+					</ThemeProvider>
+				</Auth0Provider>
+			</React.StrictMode>
+		)
+	}
+}
 
-ReactDOM.render(
-	<React.StrictMode>
-	<ThemeProvider theme={theme}>
-		<App />
-		</ThemeProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+ReactDOM.render(<Index />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
