d462b6cc14cc62f1dae25c8c5b13a4f6eda77e69
Jakub Olan <jakub.jay.olan@gmail.com>
2021-11-04 13:52:39 +0100

    build: update webpack config (MEL-4)

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,88 @@
 /* Well, it's tricky to explain, why we're really using webpack. I was using it in most of my past projects and there aren't reasons why should not use it. */
 
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
-const bar = require('webpackbar');
-const errors = require('friendly-errors-webpack-plugin');
-const nodemon = require('nodemon-webpack-plugin');
-const tspaths = require('tsconfig-paths-webpack-plugin');
+const bar = require('webpackbar')
+const errors = require('friendly-errors-webpack-plugin')
+const nodemon = require('nodemon-webpack-plugin')
+const tspaths = require('tsconfig-paths-webpack-plugin')
 
-module.exports = {
+const { ESBuildMinifyPlugin } = require('esbuild-loader')
+const { merge } = require('webpack-merge')
+
+const baseConfig = {
 	entry: './lib/index.ts',
-	mode: 'development', // TODO: Should be realtive to NODE_ENV
 	target: 'node',
-	watch: true, // TODO: Should be realtive to NODE_ENV
 	stats: 'none',
-
 	devServer: {
 		quiet: true,
 	},
-
 	module: {
 		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'esbuild-loader',
+				exclude: /node_modules/,
+				options: {
+					loader: 'ts',
+					target: 'esnext',
+				},
+			},
+			/* An alternative loader for TypeScript,
+			 * esbuild is faster than ts-loader, so we'll keep it for now.
 			{
 				test: /\.tsx?$/,
 				use: 'ts-loader',
 				exclude: /node_modules/,
 			},
+			*/
 		],
 	},
-
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
 		plugins: [new tspaths()],
 	},
-
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	externals: {
+		express: 'commonjs express',
+	},
+}
+const productionConfig = {
+	mode: 'production',
 	plugins: [
 		new bar({
 			name: 'mellstruct',
 		}),
 		new errors(),
-		new nodemon(),
 	],
-
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+	optimization: {
+		minimizer: [
+			new ESBuildMinifyPlugin({
+				target: 'es2016', // Syntax to compile to (see options below for possible values)
+			}),
+		],
 	},
-};
+}
+const developmentConfig = {
+	mode: 'development',
+	watch: true,
+	plugins: [
+		new bar({
+			name: 'mellstruct',
+		}),
+		new errors(),
+		new nodemon({
+			quiet: true,
+		}),
+	],
+}
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports = merge(baseConfig, productionConfig)
+} else {
+	module.exports = merge(baseConfig, developmentConfig)
+}
