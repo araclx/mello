7186386f51ead0b5c889d5bf122d7bd733f573ae
Jakub Olan <jakub.jay.olan@gmail.com>
2021-11-05 17:26:13 +0100

    chore: add example secure routes

--- lib/interfaces/http.ts
+++ lib/interfaces/http.ts
@@ -3,6 +3,9 @@ import compression from 'compression'
 import cors from 'cors'
 import joi from 'joi'
 
+import { auth, requiresAuth } from 'express-openid-connect'
+import { MELLO_AUTH_CLIENTID, MELLO_AUTH_SECRET } from 'utils/env'
+
 const app = express()
 
 app.use(express.json())
@@ -11,8 +14,18 @@ app.use(compression())
 app.use(cors())
 app.disable('x-powered-by')
 
-app.get('/hey', (req: express.Request, res: express.Response) => res.json({ message: 'hey' }))
+app.use(
+	auth({
+		authRequired: false,
+		auth0Logout: true,
+		secret: MELLO_AUTH_SECRET,
+		baseURL: 'http://localhost:1337',
+		clientID: MELLO_AUTH_CLIENTID,
+		issuerBaseURL: 'https://araclx.eu.auth0.com',
+	})
+)
 
+app.get('/hey', (req: express.Request, res: express.Response) => res.json({ message: 'hey' }))
 app.post('/hey', async (req: express.Request, res: express.Response) => {
 	const { error, value } = joi
 		.object({
@@ -25,6 +38,14 @@ app.post('/hey', async (req: express.Request, res: express.Response) => {
 	} else {
 		res.json({ message: `hello ${value.name}` })
 	}
+})
+
+app.get('/login-status', (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+})
+
+app.get('/profile', requiresAuth(), (req, res) => {
+	res.json(req.oidc.user)
 })
 
 export default app
--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -1,5 +1,13 @@
-/* File with easy accessible environment variables */
+import { config } from 'dotenv'
+
+config()
 
 export const NODE_ENV = process.env.NODE_ENV || 'development'
 export const HOST = process.env.HOST! || 'localhost'
 export const MELLO_PORT = Number(process.env.MELLO_PORT!) || 1337
+
+// Authentication-related Configuration
+/* To generate secure secret for auth, use command: openssl rand -hex 32 */
+export const MELLO_AUTH_SECRET = process.env.MELLO_AUTH_SECRET! || 'devisnotsecure'
+export const MELLO_AUTH_CLIENTID = process.env.MELLO_AUTH_CLIENTID!
+export const MELLO_AUTH_ISSUER = process.env.MELLO_AUTH_ISSUER!
