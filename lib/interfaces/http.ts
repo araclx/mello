5107143c8b9d49734c7894a0fd2288aa16ba41c3
Jakub Olan <jakub.jay.olan@gmail.com>
2021-11-06 04:13:48 +0100

    refactor: clean-up file structure

--- lib/interfaces/http.ts
+++ lib/interfaces/http.ts
@@ -1,10 +1,8 @@
 import express from 'express'
 import compression from 'compression'
 import cors from 'cors'
-import joi from 'joi'
 
-import { auth, requiresAuth } from 'express-openid-connect'
-import { MELLO_AUTH_CLIENTID, MELLO_AUTH_SECRET } from 'utils/env'
+import heyRouter from '../v1/hey/router'
 
 const app = express()
 
@@ -14,38 +12,6 @@ app.use(compression())
 app.use(cors())
 app.disable('x-powered-by')
 
-app.use(
-	auth({
-		authRequired: false,
-		auth0Logout: true,
-		secret: MELLO_AUTH_SECRET,
-		baseURL: 'http://localhost:1337',
-		clientID: MELLO_AUTH_CLIENTID,
-		issuerBaseURL: 'https://araclx.eu.auth0.com',
-	})
-)
-
-app.get('/hey', (req: express.Request, res: express.Response) => res.json({ message: 'hey' }))
-app.post('/hey', async (req: express.Request, res: express.Response) => {
-	const { error, value } = joi
-		.object({
-			name: joi.string().required(),
-		})
-		.validate(req.body)
-
-	if (error) {
-		res.status(400).json({ error: error.message })
-	} else {
-		res.json({ message: `hello ${value.name}` })
-	}
-})
-
-app.get('/login-status', (req, res) => {
-	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
-})
-
-app.get('/profile', requiresAuth(), (req, res) => {
-	res.json(req.oidc.user)
-})
+app.use('/v1/hey', heyRouter)
 
 export default app
