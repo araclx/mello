fa68aa7097ad47e23813f3de24cc3ccebba2419a
Jakub Olan <jakub.jay.olan@gmail.com>
2021-11-06 04:14:12 +0100

    chore: check important env vars

--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv'
+import consola from 'consola'
 
 config()
 
@@ -8,6 +9,10 @@ export const MELLO_PORT = Number(process.env.MELLO_PORT!) || 1337
 
 // Authentication-related Configuration
 /* To generate secure secret for auth, use command: openssl rand -hex 32 */
+if (process.env.MELLO_AUTH_CLIENTID! || process.env.MELLO_AUTH_ISSUER!) {
+	consola.error('MELLO_AUTH_CLIENTID and MELLO_AUTH_ISSUER must be set in .env file')
+}
+
 export const MELLO_AUTH_SECRET = process.env.MELLO_AUTH_SECRET! || 'devisnotsecure'
 export const MELLO_AUTH_CLIENTID = process.env.MELLO_AUTH_CLIENTID!
 export const MELLO_AUTH_ISSUER = process.env.MELLO_AUTH_ISSUER!
