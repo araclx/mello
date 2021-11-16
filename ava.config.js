7f01417116dbecabfd7fbefca2c0aaed06473788
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-11-16 04:49:04 +0100

    ci: add tsconfig-paths

--- ava.config.js
+++ ava.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	extensions: ['ts'],
-	require: ['ts-node/register'],
+	require: ['ts-node/register', 'tsconfig-paths/register'],
 	failFast: true,
 	concurrency: 5,
 	files: ['__tests__/**/*'],
