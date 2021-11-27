de8ffa5e45e8dd4f0f3758bcde7f78516b86ed30
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-11-27 12:48:56 +0100

    chore: add profile route (MEL-12) (#31)

--- lib/auth/router.v1.ts
+++ lib/auth/router.v1.ts
@@ -21,4 +21,13 @@ router.post('/login', (req, res, next) => {
 		})
 	})(req, res, next)
 })
+
+router.get('/profile', passport.authenticate('local'), (req, res) => {
+	if (req.isAuthenticated()) {
+		res.status(200).json(req.user)
+	} else {
+		res.status(404)
+	}
+})
+
 export default router
