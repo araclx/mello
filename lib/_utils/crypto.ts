3fb7b5592346a526d186db819f60d47f5acd95a9
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-12-02 18:53:55 +0100

    chore: cybersec is my passion

--- lib/_utils/crypto.ts
+++ lib/_utils/crypto.ts
@@ -1,17 +1,18 @@
 import argon2 from 'argon2'
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 
 export async function hash(text: string) {
-	const prehashstr = await bcrypt.hash(text, 10)
-	const hashstr = await argon2.hash(prehashstr)
-	return hashstr
+	// const prehashstr = await bcrypt.hash(text, 10)
+	// const hashstr = await argon2.hash(prehashstr)
+	// return hashstr
+	return argon2.hash(text)
 }
 
 export async function verify(text: string, hash: string) {
-	const precompare = await argon2.verify(hash, text)
-	if (!precompare) {
-		return false
-	} else {
-		return await bcrypt.compare(text, precompare)
-	}
+	return await argon2.verify(hash, text)
+	// if (!precompare) {
+	// 	return false
+	// } else {
+	// 	return await bcrypt.compare(text, precompare)
+	// }
 }
