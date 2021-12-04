5f9511240a11070b4388c1b632bad815a9d38acb
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-12-04 11:16:22 +0100

    utils: update helper functions

--- lib/_utils/funs.ts
+++ lib/_utils/funs.ts
@@ -1,11 +1,11 @@
 import { NODE_ENV } from './env'
 
 export function isDev() {
-	if (NODE_ENV === 'development') true
+	if (NODE_ENV == 'development') true
 	else false
 }
 
 export function isDevOrCI() {
-	if (NODE_ENV === 'development' || NODE_ENV === 'ci') true
+	if (NODE_ENV == 'development' || NODE_ENV == 'CI') true
 	else false
 }
