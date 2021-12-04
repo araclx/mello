35f958660b024642005d955a2fa4fb8b5fab8260
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-12-04 09:59:04 +0100

    fix: typo in user model

--- lib/users/model.ts
+++ lib/users/model.ts
@@ -6,12 +6,11 @@ const schema = new Schema({
 	email: { type: String, unique: true, required: true },
 	username: { type: String, unique: true, required: true, min: 6, max: 32 },
 	password: String,
-	salt: String,
 	birthdate: Date,
 	height: Number,
 	weight: Number,
 	gender: String,
-	profilePictire: String,
+	profilePicture: String,
 	photos: [String],
 	location: String,
 })
