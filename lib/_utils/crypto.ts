9c69d5766e52ba0d7427bb2911c0a18a2399d696
Jay Whitewood <jakub.jay.olan@gmail.com>
2021-12-08 14:16:40 +0100

    style: clean up comments

--- lib/_utils/crypto.ts
+++ lib/_utils/crypto.ts
@@ -2,17 +2,9 @@ import argon2 from 'argon2'
 // import bcrypt from 'bcrypt'
 
 export async function hash(text: string) {
-	// const prehashstr = await bcrypt.hash(text, 10)
-	// const hashstr = await argon2.hash(prehashstr)
-	// return hashstr
 	return argon2.hash(text)
 }
 
 export async function verify(text: string, hash: string) {
 	return await argon2.verify(hash, text)
-	// if (!precompare) {
-	// 	return false
-	// } else {
-	// 	return await bcrypt.compare(text, precompare)
-	// }
 }
